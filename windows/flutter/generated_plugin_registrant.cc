14923e5c54f1f1ffe000dd2c93e85cf5159f7ead
OnlyXianzo <184820152+OnlyXianzo@users.noreply.github.com>
2026-06-08 18:27:24 +0530

    ci: restructure verify/build workflows and update pubspec dependencies

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,10 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <connectivity_plus/connectivity_plus_windows_plugin.h>
 #include <flutter_angle/flutter_angle_plugin.h>
 #include <rive_native/rive_native_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
   FlutterAnglePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FlutterAnglePlugin"));
   RiveNativePluginRegisterWithRegistrar(
